069e26fda92a6ddb6462bcf9b2b1c569e881dc31
BLYTH Mark UNIVERSITE BRISTOL <MB279693@intra.cea.fr>
2024-07-31 11:15:08 +0200

    Add fixedtemplut to namespace

--- ecm1d/__init__.py
+++ ecm1d/__init__.py
@@ -2,3 +2,4 @@ from .kokamlut import KokamParameters
 from .ideallut import IdealisedParameters
 from .ecm import ECM, BaseParameters
 from .lumped import HomogenousECM
+from .fixedtemplut import FixedTemperatureLUT
